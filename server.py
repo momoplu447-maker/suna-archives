8f77bf065b4c58135d4b3c6198088bbd52bbf9bf
Aziz <momoplu447@gmail.com>
2026-05-12 03:31:44 +0200

    Use environment variable for server port

--- server.py
+++ server.py
@@ -227,4 +227,5 @@ if __name__ == '__main__':
     print("║  Admin        →  http://localhost:5000/admin ║")
     print("║  API          →  http://localhost:5000/api   ║")
     print("╚══════════════════════════════════════════╝")
-    app.run(host='0.0.0.0', port=5000, debug=True)
+   port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)

28d9f9f3d128651fd258f7c98a1fd83fa37ed1c8
Aziz <momoplu447@gmail.com>
2026-05-12 03:46:52 +0200

    Update server.py

--- server.py
+++ server.py
@@ -227,5 +227,5 @@ if __name__ == '__main__':
     print("║  Admin        →  http://localhost:5000/admin ║")
     print("║  API          →  http://localhost:5000/api   ║")
     print("╚══════════════════════════════════════════╝")
-   port = int(os.environ.get('PORT', 5000))
+port = int(os.environ.get('PORT', 5000))
 app.run(host='0.0.0.0', port=port)
